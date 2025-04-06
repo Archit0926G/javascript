17256cf936b49b4221c7a3217bfb414d182c4046
Archit0926G <154497532+Archit0926G@users.noreply.github.com>
2025-04-06 16:47:28 +0530

    Add files via upload

---
+++ 02_basic/spread_operator.js
@@ -0,0 +1,14 @@
+const myArr = [1,2,3,4,5,6,7,8,9,10,64]
+
+const maximum = Math.max(...myArr)
+const minimum = Math.min(...myArr)
+const sqrt = Math.sqrt(myArr[10])
+
+console.log(maximum)
+console.log(minimum)
+console.log(sqrt)
+
+const myName = "Archit"
+const myNamearr = [...myName]
+
+console.log(myNamearr)

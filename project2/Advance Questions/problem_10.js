c0c67b38758850010003c6a5ae629d7aeac3915d
Archit0926G <154497532+Archit0926G@users.noreply.github.com>
2025-05-12 11:56:53 +0530

    Update problem_10.js

--- project2/Advance Questions/problem_10.js
+++ project2/Advance Questions/problem_10.js
@@ -1,2 +1,26 @@
 //problem statement :- Determine whether a given array is sorted in ascending order, descending order, or unsorted
 
+let arr = [5,3,4,2,1]
+let ascending = true
+let descending = true
+
+for (let i = 0; i < arr.length-1; i++) {
+    
+    if (arr[i] > arr[i+1]) {
+        ascending = false
+    }
+    else if (arr[i] < arr[i+1]) {
+        descending = false
+    }
+    
+}
+
+if (ascending == true) {
+    console.log(`${arr} is in ascending order`)
+}
+else if (descending == true) {
+    console.log(`${arr} is in descending order`)
+}
+else{
+    console.log(`${arr} is unsorted`)
+}
